08eda9f0673839d263e8690f4f9bcc0cd7619d84
Ignacio Contreras <ignaciocontreras.iacne@gmail.com>
2024-06-25 17:10:15 -0400

    Funcion para generar y clasificar saldos

--- ContrerasIgnacio.py
+++ ContrerasIgnacio.py
@@ -2,3 +2,18 @@
 import random
 import statistics
 import csv
+
+# Funcion para generar y clasificar saldos
+
+def generar_y_clasificar_saldos():
+    saldos = [random.randint(1, 10000) for _ in range(10)]
+    rangos = {"Saldo Bajo": [], "Saldo Medio": [], "Saldo Alto": []}
+    for saldo in saldos:
+        if saldos < 3000:
+            rangos["Saldo Bajo"].append(saldo)
+        elif saldo < 6000:
+            rangos["Saldo Medio"].append(saldo)
+        else:
+            rangos["Saldo Alto"].append(saldo)
+    return saldos, rangos
+
